b08ccb4d565c97d33f3dabbef0c839e000b81305
Johannes Zellner <johannes@nebulon.de>
2018-02-18 04:18:56 -0800

    rebuild on start

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ if [[ ! -d $REPO_PATH ]]; then
     echo "Push to https://blabla" > $WEBSITE_PATH/index.html
 else
     echo "=> Building pages"
-    # /usr/local/bin/gosu cloudron:cloudron /app/code/build-pages.sh
+    /usr/local/bin/gosu cloudron:cloudron /app/code/build-pages.sh
 fi
 
 echo "=> Ensure git hook"
